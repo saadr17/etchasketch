c565055f3fce7fd8df574e9ef9acee7444096e78
Saad <shraja7@gmail.com>
2022-03-01 10:42:19 -0600

    rework app and add reset feature

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
-let height = 16; // number of guesses
-let width = 16;
+const grid = document.querySelector(".gridContainer");
+const userInput = document.getElementById("quantity");
+const resetButton = document.querySelector(".reset");
 
-const clearButton = document.querySelector("#clearButton");
-
-window.onload = function () {
-  initialize();
-};
-
-const initialize = () => {
-  const board = document.getElementById("board");
-  board.style.width = `${width * 36.6}px`;
-
-  for (let r = 0; r < height; r++) {
-    for (let c = 0; c < width; c++) {
-      //creating a span and inserting id and class to that
-      let tile = document.createElement("span");
-      tile.id = r.toString() + "-" + c.toString();
-      tile.classList.add("tile");
-      tile.innerText = " ";
-      document.getElementById("board").appendChild(tile);
-    }
+createGrid = () => {
+  for (let i = 0; i < 256; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
   }
-
-  //listen for mouserover
-  board.addEventListener("mouseover", (e) => {
-    e.target.classList.add("blue");
-  });
-  const span = document.querySelectorAll(".tile");
-  clearButton.addEventListener("click", (e) => {
-    // span.classList.add("reset");
-    for (const s of span) {
-      s.classList.remove("blue");
-    }
-  });
 };
+
+updateGrid = () => {
+  // grid.innerHTML = "";
+  grid.style.setProperty(
+    "grid-template-columns",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  grid.style.setProperty(
+    "grid-template-rows",
+    `repeat(${userInput.value}, 2fr)`
+  );
+  for (let i = 0; i < userInput.value * userInput.value; i++) {
+    const div = document.createElement("div");
+    div.classList.add("square");
+    grid.appendChild(div);
+  }
+  console.log(userInput.value);
+};
+
+const square = document.querySelector("div");
+square.addEventListener("mouseover", function (event) {
+  event.target.classList.replace("square", "color");
+});
+
+userInput.addEventListener("change", updateGrid);
+
+resetButton.addEventListener("click", function () {
+  grid.innerHTML = "";
+  userInput.value = "";
+  grid.style.setProperty("grid-template-columns", `repeat(16, 2fr)`);
+  grid.style.setProperty("grid-template-rows", `repeat(16, 2fr)`);
+  createGrid();
+});
+
+createGrid();
